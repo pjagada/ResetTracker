3b2e4052db206eafbb6f27033a7de0bfac48d817
pjagada <pranavj918@gmail.com>
2022-06-15 17:28:34 -0500

    changed what is checked for

--- ResetTracker/checks.py
+++ ResetTracker/checks.py
@@ -1,23 +1,12 @@
 advChecks = [
-    ("minecraft:recipes/misc/charcoal", "has_log"),
+    ("minecraft:recipes/decorations/light_gray_bed_from_white_bed", "has_bed"),
     ("minecraft:story/iron_tools", "iron_pickaxe"),
+    ("minecraft:recipes/decorations/fletching_table", "has_flint"),
     ("timelines", "enter_nether"),
-    ("timelines", "enter_bastion"),
-    ("timelines", "enter_fortress"),
     ("timelines", "nether_travel"),
-    ("timelines", "enter_stronghold"),
     ("timelines", "enter_end"),
 ]
 
 statsChecks = [
     "nothing lol",
-    ("minecraft:dropped", "minecraft:gold_ingot"),
-    ("minecraft:picked_up", "minecraft:blaze_rod"),
-    ("minecraft:killed", "minecraft:blaze"),
-    ("minecraft:picked_up", "minecraft:flint"),
-    ("minecraft:mined", "minecraft:gravel"),
-    ("minecraft:custom", "minecraft:deaths"),
-    ("minecraft:custom", "minecraft:traded_with_villager"),
-    ("minecraft:killed", "minecraft:enderman"),
-    ("minecraft:picked_up", "minecraft:ender_eye"),
 ]
